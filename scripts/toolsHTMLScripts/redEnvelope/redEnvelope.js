bc53d1a8aa880ef7aa6bca688033df191942e118
kriYamiHikari <89930005+kriYamiHikari@users.noreply.github.com>
2022-06-16 11:19:35 +0800

    update
    小应用内红包抽奖功能界面优化
    红包抽奖新增序号生成红包功能
    红包生成数量上限调整为400个

--- scripts/toolsHTMLScripts/redEnvelope/redEnvelope.js
+++ scripts/toolsHTMLScripts/redEnvelope/redEnvelope.js
@@ -1,5 +1,6 @@
 function main(){
     setInterval(events,1)
+    let isInputMode = true
     let row=document.getElementById('envelopeContainer')
     let originText='            <div class="redEnvelopeItem">\n' +
         '                <img src="redEnvelope.png" class="redEnvelopeItemCloseImg" alt="图片加载失败">\n' +
@@ -20,11 +21,50 @@ function main(){
             }
         }
     }
-
+    //点击生成红包按钮
     document.getElementById('send').onclick=function () {
-        let text=document.getElementById('redEnvelopeInputTextarea').value
-        let items=text.match(/.*?;/g)
-        if (items!=null){
+        let error = false
+        let items = []
+        //添加物品到数组
+        if (isInputMode === true) {
+            let text=document.getElementById('redEnvelopeInputTextarea').value
+            items = text.match(/(.*?)[|]/g)
+            if (items.length < 400){
+                for (let a=0; a<items.length; a++){
+                    items[a] = items[a].replace(/[|]/g,"")
+                }
+            }else{
+                alert("物品数量不能大于400个！")
+            }
+        }
+        if (isInputMode === false) {
+            let minNumber = parseInt(document.getElementById('redEnvelopeRangeMinTextarea').value)
+            let maxNumber = parseInt(document.getElementById('redEnvelopeRangeMaxTextarea').value)
+            if (isNaN(minNumber)){
+                alert("最小值输入错误")
+                error = true
+            }
+            if (isNaN(maxNumber)){
+                alert("最大值输入错误")
+                error = true
+            }
+            if (minNumber > maxNumber){
+                alert("最小值不可以超过最大值")
+                error = true
+            }
+            if (error !== true){
+                let itemCount = maxNumber - minNumber
+                if (itemCount < 1000){
+                    for (let n = minNumber; n < maxNumber+1;n++){
+                        items.push(n.toString())
+                    }
+                }else{
+                    alert("物品数量不能大于400个！")
+                }
+            }
+        }
+        //根据数组元素生成红包
+        if (items != null){
             row.innerHTML=""
 
             for (let b=0;b<items.length;b++){
@@ -40,6 +80,19 @@ function main(){
                 row.children[b].children[1].getElementsByTagName("div")[0].innerHTML=newArr[b].replace(";","")
             }
         }
+    }
+
+    document.getElementById('inputItemMode').onclick = function () {
+        isInputMode = true
+        document.getElementById('redEnvelopeRangeMode').className = "hidden"
+        document.getElementById('redEnvelopeInputMode').className = "showAnimation"
+        document.getElementById('envelopeContainer').innerHTML = ""
+    }
+    document.getElementById('rangeNumberItemMode').onclick = function () {
+        isInputMode = false
+        document.getElementById('redEnvelopeRangeMode').className = "showAnimation"
+        document.getElementById('redEnvelopeInputMode').className = "hidden"
+        document.getElementById('envelopeContainer').innerHTML = ""
     }
 }
 main()
